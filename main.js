ad3da9162e3bc35719c2518845c9a090b2d14ed9
radriann21 <adrianrangelargu@gmail.com>
2024-09-26 11:46:22 -0400

    finished basic funcionality

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const box = document.getElementById('box')
+const inputs = document.querySelectorAll('input')
 
+const objectMap = {
+  topl: 'borderTopLeftRadius',
+  topr: 'borderTopRightRadius',
+  bottoml: 'borderBottomLeftRadius',
+  bottomr: 'borderBottomRightRadius'
+}
+
+inputs.forEach(input => {
+  input.addEventListener('input', (evt) => {
+    const property = objectMap[input.getAttribute('name')]
+    if (property) {
+      box.style[property] = `${evt.target.value}px`
+    }
+  })
+})
